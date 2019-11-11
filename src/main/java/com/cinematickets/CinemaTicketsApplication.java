c265af019478e5fc034fd3952c94c0bfbf75e6a0
Ahmad Syarifuddin <udin@gaeasystem.com>
2019-11-11 10:37:31 +0700

    start init new project

---
+++ src/main/java/com/cinematickets/CinemaTicketsApplication.java
@@ -0,0 +1,13 @@
+package com.cinematickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CinemaTicketsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CinemaTicketsApplication.class, args);
+	}
+
+}
